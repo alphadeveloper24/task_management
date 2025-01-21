f27bd66ee71e7d686d7517049fa33ae9ddee63c0
varun <uvarun24@gmail.com>
2025-01-21 23:26:42 +0530

    changed links

--- context/userContext.js
+++ context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://task-management-backend-9cd8.onrender.com";
 
   const router = useRouter();
 
